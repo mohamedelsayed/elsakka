88a391e59a35baa70d643ce9a4ac931338737947
Mohamed Elsayed <m.elsayed@shiftebusiness.com>
2017-01-25 19:19:54 +0200

    shift

--- sites/all/themes/front/js/custom.js
+++ sites/all/themes/front/js/custom.js
@@ -52,3 +52,14 @@ jQuery(document).ready(function(){
         ]
     });
 });
+jQuery(document).ready(function() {
+/*
+	var defaults = {
+	containerID: 'toTop', // fading element id
+	containerHoverID: 'toTopHover', // fading element hover id
+	scrollSpeed: 1200,
+	easingType: 'linear' 
+	};
+*/								
+jQuery().UItoTop({ easingType: 'easeOutQuart' });
+});
--- sites/all/themes/front/templates/footer.php
+++ sites/all/themes/front/templates/footer.php
@@ -53,19 +53,6 @@
 <div id="Developer">
     Developed by <a href="http://www.mohamedelsayed.net" target="_blank">Mohamed Elsayed</a>
 </div>
-<!-- smooth scrolling -->
-	<script type="text/javascript">
-		$(document).ready(function() {
-		/*
-			var defaults = {
-			containerID: 'toTop', // fading element id
-			containerHoverID: 'toTopHover', // fading element hover id
-			scrollSpeed: 1200,
-			easingType: 'linear' 
-			};
-		*/								
-		$().UItoTop({ easingType: 'easeOutQuart' });
-		});
-	</script>
-	<a href="#" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+
+<a href="#" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
 <!-- //smooth scrolling -->
