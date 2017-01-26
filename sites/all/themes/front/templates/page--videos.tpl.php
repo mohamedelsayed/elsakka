113aa657f8d50572e7d1e1d050fe26738e215a9a
Mohamed Elsayed <m.elsayed@shiftebusiness.com>
2017-01-26 02:31:28 +0200

    shift

---
+++ sites/all/themes/front/templates/page--videos.tpl.php
@@ -0,0 +1,14 @@
+<?php /**
+ * @author Author "Mohamed Elsayed"  
+ * @author Author Email "me@mohamedelsayed.net"
+ * @link http://www.mohamedelsayed.net
+ * @copyright Copyright (c) 2017 Programming by "http://www.mohamedelsayed.net"
+ */?>
+<?php global $base_url;?>    
+<?php include_once 'header.php';?>
+<div class="facilities">
+	<div class="container">
+		videos
+	</div>
+</div>
+<?php include_once 'footer.php';?>
