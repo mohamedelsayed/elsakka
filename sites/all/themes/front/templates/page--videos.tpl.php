1a622ac6045053611f1f9597e86550e70cb1540f
Ahmed ELsayed <Ahmed ELsayed>
2017-01-28 15:11:07 +0200

    shift

--- sites/all/themes/front/templates/page--videos.tpl.php
+++ sites/all/themes/front/templates/page--videos.tpl.php
@@ -14,46 +14,7 @@
                 	<div class="grid">
 						<a href="images/1.mp4" class="lsb-preview" data-lsb-group="header">
 							<figure class="effect-winston">
-								<img src="images/4.jpg" class="img-responsive" alt=" " />
-								<figcaption>
-									<p>
-										<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
-										<span class="glyphicon glyphicon-heart" aria-hidden="true"></span>
-										<span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
-									</p>
-								</figcaption>			
-							</figure>
-						</a>
-					</div>
-				</div>
-				<div class="col-md-4 w3_tabs_grid">
-					<div class="grid">
-						<a href="images/5.jpg" class="lsb-preview" data-lsb-group="header">
-							<figure class="effect-winston">
-								<img src="images/5.jpg" class="img-responsive" alt=" " />
-								<figcaption>
-									<p>
-										<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
-										<span class="glyphicon glyphicon-heart" aria-hidden="true"></span>
-										<span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
-									</p>
-								</figcaption>			
-							</figure>
-						</a>
-					</div>
-				</div>
-				<div class="col-md-4 w3_tabs_grid">
-					<div class="grid">
-						<a href="images/6.jpg" class="lsb-preview" data-lsb-group="header">
-							<figure class="effect-winston">
-								<img src="images/6.jpg" class="img-responsive" alt=" " />
-								<figcaption>
-									<p>
-										<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
-										<span class="glyphicon glyphicon-heart" aria-hidden="true"></span>
-										<span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
-									</p>
-								</figcaption>			
+								<img src="<?php echo $base_url.'/'.path_to_theme();?>/img/16.jpg" class="img-responsive" alt=" " />
 							</figure>
 						</a>
 					</div>
@@ -62,14 +23,7 @@
 					<div class="grid">
 						<a href="images/13.jpg" class="lsb-preview" data-lsb-group="header">
 							<figure class="effect-winston">
-								<img src="images/13.jpg" class="img-responsive" alt=" " />
-								<figcaption>
-									<p>
-										<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
-										<span class="glyphicon glyphicon-heart" aria-hidden="true"></span>
-										<span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
-									</p>
-								</figcaption>			
+								<img src="<?php echo $base_url.'/'.path_to_theme();?>/img/17.jpg" class="img-responsive" alt=" " />			
 							</figure>
 						</a>
 					</div>
@@ -78,14 +32,8 @@
 					<div class="grid">
 						<a href="images/14.jpg" class="lsb-preview" data-lsb-group="header">
 							<figure class="effect-winston">
-								<img src="images/14.jpg" class="img-responsive" alt=" " />
-								<figcaption>
-									<p>
-										<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
-										<span class="glyphicon glyphicon-heart" aria-hidden="true"></span>
-										<span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
-									</p>
-								</figcaption>			
+								<img src="<?php echo $base_url.'/'.path_to_theme();?>/img/18.jpg" class="img-responsive" alt=" " />
+										
 							</figure>
 						</a>
 					</div>
@@ -94,30 +42,7 @@
 					<div class="grid">
 						<a href="images/15.jpg" class="lsb-preview" data-lsb-group="header">
 							<figure class="effect-winston">
-								<img src="images/15.jpg" class="img-responsive" alt=" " />
-								<figcaption>
-									<p>
-										<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
-										<span class="glyphicon glyphicon-heart" aria-hidden="true"></span>
-										<span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
-									</p>
-								</figcaption>			
-							</figure>
-						</a>
-					</div>
-				</div>
-				<div class="col-md-4 w3_tabs_grid">
-					<div class="grid">
-						<a href="images/18.jpg" class="lsb-preview" data-lsb-group="header">
-							<figure class="effect-winston">
-								<img src="images/18.jpg" class="img-responsive" alt=" " />
-								<figcaption>
-									<p>
-										<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
-										<span class="glyphicon glyphicon-heart" aria-hidden="true"></span>
-										<span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
-									</p>
-								</figcaption>			
+								<img src="<?php echo $base_url.'/'.path_to_theme();?>/img/3.jpg" class="img-responsive" alt=" " />			
 							</figure>
 						</a>
 					</div>
@@ -126,14 +51,7 @@
 					<div class="grid">
 						<a href="images/17.jpg" class="lsb-preview" data-lsb-group="header">
 							<figure class="effect-winston">
-								<img src="images/17.jpg" class="img-responsive" alt=" " />
-								<figcaption>
-									<p>
-										<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
-										<span class="glyphicon glyphicon-heart" aria-hidden="true"></span>
-										<span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
-									</p>
-								</figcaption>			
+								<img src="<?php echo $base_url.'/'.path_to_theme();?>/img/5.jpg" class="img-responsive" alt=" " />		
 							</figure>
 						</a>
 					</div>
@@ -142,14 +60,7 @@
 					<div class="grid">
 						<a href="images/16.jpg" class="lsb-preview" data-lsb-group="header">
 							<figure class="effect-winston">
-								<img src="images/16.jpg" class="img-responsive" alt=" " />
-								<figcaption>
-									<p>
-										<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
-										<span class="glyphicon glyphicon-heart" aria-hidden="true"></span>
-										<span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
-									</p>
-								</figcaption>			
+								<img src="<?php echo $base_url.'/'.path_to_theme();?>/img/6.jpg" class="img-responsive" alt=" " />		
 							</figure>
 						</a>
 					</div>
