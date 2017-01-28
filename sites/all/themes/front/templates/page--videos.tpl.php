3c40d1ef8c908834163d21bbe9289cd9eb1a46ae
Ahmed ELsayed <Ahmed ELsayed>
2017-01-28 14:21:04 +0200

    shift

--- sites/all/themes/front/templates/page--videos.tpl.php
+++ sites/all/themes/front/templates/page--videos.tpl.php
@@ -7,8 +7,162 @@
 <?php global $base_url;?>    
 <?php include_once 'header.php';?>
 <div class="facilities">
-	<div class="container">
-		videos
+	<div class="gallery">
+		<div class="container">
+			<div class="agileinfo_gallery_grids">
+				<div class="col-md-4 w3_tabs_grid">
+                	<div class="grid">
+						<a href="images/1.mp4" class="lsb-preview" data-lsb-group="header">
+							<figure class="effect-winston">
+								<img src="images/4.jpg" class="img-responsive" alt=" " />
+								<figcaption>
+									<p>
+										<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+										<span class="glyphicon glyphicon-heart" aria-hidden="true"></span>
+										<span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
+									</p>
+								</figcaption>			
+							</figure>
+						</a>
+					</div>
+				</div>
+				<div class="col-md-4 w3_tabs_grid">
+					<div class="grid">
+						<a href="images/5.jpg" class="lsb-preview" data-lsb-group="header">
+							<figure class="effect-winston">
+								<img src="images/5.jpg" class="img-responsive" alt=" " />
+								<figcaption>
+									<p>
+										<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+										<span class="glyphicon glyphicon-heart" aria-hidden="true"></span>
+										<span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
+									</p>
+								</figcaption>			
+							</figure>
+						</a>
+					</div>
+				</div>
+				<div class="col-md-4 w3_tabs_grid">
+					<div class="grid">
+						<a href="images/6.jpg" class="lsb-preview" data-lsb-group="header">
+							<figure class="effect-winston">
+								<img src="images/6.jpg" class="img-responsive" alt=" " />
+								<figcaption>
+									<p>
+										<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+										<span class="glyphicon glyphicon-heart" aria-hidden="true"></span>
+										<span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
+									</p>
+								</figcaption>			
+							</figure>
+						</a>
+					</div>
+				</div>
+				<div class="col-md-4 w3_tabs_grid w3_tabs_grid_sub">
+					<div class="grid">
+						<a href="images/13.jpg" class="lsb-preview" data-lsb-group="header">
+							<figure class="effect-winston">
+								<img src="images/13.jpg" class="img-responsive" alt=" " />
+								<figcaption>
+									<p>
+										<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+										<span class="glyphicon glyphicon-heart" aria-hidden="true"></span>
+										<span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
+									</p>
+								</figcaption>			
+							</figure>
+						</a>
+					</div>
+				</div>
+				<div class="col-md-4 w3_tabs_grid w3_tabs_grid_sub">
+					<div class="grid">
+						<a href="images/14.jpg" class="lsb-preview" data-lsb-group="header">
+							<figure class="effect-winston">
+								<img src="images/14.jpg" class="img-responsive" alt=" " />
+								<figcaption>
+									<p>
+										<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+										<span class="glyphicon glyphicon-heart" aria-hidden="true"></span>
+										<span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
+									</p>
+								</figcaption>			
+							</figure>
+						</a>
+					</div>
+				</div>
+				<div class="col-md-4 w3_tabs_grid w3_tabs_grid_sub">
+					<div class="grid">
+						<a href="images/15.jpg" class="lsb-preview" data-lsb-group="header">
+							<figure class="effect-winston">
+								<img src="images/15.jpg" class="img-responsive" alt=" " />
+								<figcaption>
+									<p>
+										<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+										<span class="glyphicon glyphicon-heart" aria-hidden="true"></span>
+										<span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
+									</p>
+								</figcaption>			
+							</figure>
+						</a>
+					</div>
+				</div>
+				<div class="col-md-4 w3_tabs_grid">
+					<div class="grid">
+						<a href="images/18.jpg" class="lsb-preview" data-lsb-group="header">
+							<figure class="effect-winston">
+								<img src="images/18.jpg" class="img-responsive" alt=" " />
+								<figcaption>
+									<p>
+										<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+										<span class="glyphicon glyphicon-heart" aria-hidden="true"></span>
+										<span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
+									</p>
+								</figcaption>			
+							</figure>
+						</a>
+					</div>
+				</div>
+				<div class="col-md-4 w3_tabs_grid">
+					<div class="grid">
+						<a href="images/17.jpg" class="lsb-preview" data-lsb-group="header">
+							<figure class="effect-winston">
+								<img src="images/17.jpg" class="img-responsive" alt=" " />
+								<figcaption>
+									<p>
+										<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+										<span class="glyphicon glyphicon-heart" aria-hidden="true"></span>
+										<span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
+									</p>
+								</figcaption>			
+							</figure>
+						</a>
+					</div>
+				</div>
+				<div class="col-md-4 w3_tabs_grid">
+					<div class="grid">
+						<a href="images/16.jpg" class="lsb-preview" data-lsb-group="header">
+							<figure class="effect-winston">
+								<img src="images/16.jpg" class="img-responsive" alt=" " />
+								<figcaption>
+									<p>
+										<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+										<span class="glyphicon glyphicon-heart" aria-hidden="true"></span>
+										<span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
+									</p>
+								</figcaption>			
+							</figure>
+						</a>
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+			<script src="js/lsb.min.js"></script>
+			<script>
+			$(window).load(function() {
+				  $.fn.lightspeedBox();
+				});
+			</script>
+		</div>
 	</div>
 </div>
 <?php include_once 'footer.php';?>
