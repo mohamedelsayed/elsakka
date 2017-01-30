1eed6525843f5eacfa0332bf5cdd796131d754ea
Ahmed ELsayed <Ahmed ELsayed>
2017-01-30 23:10:17 +0200

    shift

--- sites/all/themes/front/templates/page--access-denied.tpl.php
+++ sites/all/themes/front/templates/page--access-denied.tpl.php
@@ -7,7 +7,7 @@
 <?php global $base_url;?>    
 <?php include_once 'common'.DS.'header.php';?>
 <div class="container" style="width: 100%;margin: auto;min-height: 450px;">
-    <div class="custom_access_denied" style="width: 200px;margin-right: auto; margin-left: auto;margin-top: 30px;">
+    <div class="custom_access_denied" style="width: 200px;margin-right: auto; margin-left: auto;">
         <img style="width: 100%;max-height: 100%;margin-right: auto; margin-left: auto;" class="" src="<?php echo $GLOBALS['base_url'].'/'.path_to_theme();?>/img/logo-accessdenied.jpg" />
     </div>
     <div class="cutom_login_form">
