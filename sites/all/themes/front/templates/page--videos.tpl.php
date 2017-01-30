fee74e9b63be5c964ed3c6d8875b8efbc8c2a16e
Mohamed Elsayed <m.elsayed@shiftebusiness.com>
2017-01-31 01:41:45 +0200

    shift

--- sites/all/themes/front/templates/page--videos.tpl.php
+++ sites/all/themes/front/templates/page--videos.tpl.php
@@ -6,56 +6,56 @@
  */?>
 <?php global $base_url;?>    
 <?php include_once 'common'.DS.'header.php';?>
+<?php $page_title = __('مكتبة الفيديو');
+drupal_set_title($page_title);?>
+<div id="mesagepopboxpopoup" class="mesage-pop" >
+	<div id="mesagecontent">
+		<h4><div class="vidoe_popup_title"></div><div id="closepopoup" class="closepopoup">X</div></h4>
+		<div id="mesagecontent_in">
+		</div>
+	</div>
+	<div class="mesage-pop-bg"></div>
+</div>
 <div class="facilities">
 	<div class="gallery">
 		<div class="container">
 			<div class="agileinfo_gallery_grids">
-                       <h3 class="tittle">مكتبة الفديو </h3>
-                       <div id="myBtn" class="view_1 view_1-eighth filimg col-md-4 col-sm-4 col-xs-12" data-myorder="2">
-						<a  href="#'">
-				    		<img src="<?php echo $base_url.'/'.path_to_theme();?>/img/16.jpg" class="img-responsive" alt=" " />
-			    		</a>
-			    		<div class="mask">
-			    			<a title="" href="#" onclick="open_popup();">
-			    				<h4>منتج</h4>
-			    			</a>
-		    			</div>                     
-                    <div id="mesagepopboxpopoup" class="mesage-pop" >
-					    <div id="mesagecontent">      <h4><div id="closepopoup" class="closepopoup">X</div></h4><div class="loading"></div>
-					                      <iframe src="https://www.youtube.com/embed/rjjgaYIhi8Y" allowfullscreen="" width="100%" height="100%" frameborder="0"></iframe>
-					</div>
-					    <div class="mesage-pop-bg"></div>
-					</div>  
-	    			</div>
-				
-                   
-				
-				
-				
+				<h3 class="tittle"><?php echo $page_title;?></h3>
+				<?php $home = 0;$limit = 3;$page = 1;$category = 0;
+		        if(isset($_GET['page'])){
+		            $page = $_GET['page'];
+		        }
+		        $return = elsayed_get_videos($category, $home, $limit, $page);
+		        $page_count = $return['page_count'];
+		        $videos = $return['items'];?>
+		        <?php include_once 'list_videos.php';?>				
 				<div class="clearfix"> </div>
 			</div>
-			<script>
-			function open_popup(){
-    //jQuery("#mesagepopboxpopoup #mesagecontent").html(content); 
-    jQuery("#mesagepopboxpopoup").addClass("alert"); 
-    jQuery("#mesagepopboxpopoup").show();
-    jQuery('body').addClass("mobile-menu-opend");       
-}
-function close_popup(){
-    jQuery("#mesagepopboxpopoup").hide(); 
-    //jQuery("#mesagepopboxpopoup #mesagecontent").html('');
-    jQuery("#mesagepopboxpopoup").removeClass("alert");
-    jQuery('body').removeClass("mobile-menu-opend");          
-}
-jQuery(document).ready(function() {
-    jQuery("#mesagepopboxpopoup").on("click",".closepopoup", function(){
-        close_popup();            
-    });
-    jQuery('.mesage-pop-bg').click(function(){
-        close_popup();
-    });
-});
-</script>
+			<script type="text/javascript">
+			function open_popup(content_class, vidoe_popup_title){
+				var content = jQuery('.'+content_class).html();
+				jQuery("#mesagepopboxpopoup #mesagecontent_in").html(content); 
+				jQuery('.vidoe_popup_title').html(vidoe_popup_title);
+				jQuery("#mesagepopboxpopoup").addClass("alert"); 
+				jQuery("#mesagepopboxpopoup").show();
+				jQuery('body').addClass("mobile-menu-opend");       
+			}
+			function close_popup(){
+				jQuery("#mesagepopboxpopoup #mesagecontent_in").html('');
+				jQuery('.vidoe_popup_title').html('');
+				jQuery("#mesagepopboxpopoup").hide(); 				
+				jQuery("#mesagepopboxpopoup").removeClass("alert");
+				jQuery('body').removeClass("mobile-menu-opend");          
+			}
+			jQuery(document).ready(function() {
+				jQuery("#mesagepopboxpopoup").on("click",".closepopoup", function(){
+					close_popup();            
+				});
+				jQuery('.mesage-pop-bg').click(function(){
+					close_popup();
+				});
+			});
+			</script>
 		</div>
 	</div>
 </div>
