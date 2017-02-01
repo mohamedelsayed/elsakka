973c7f6d052c910f0601b17a53504239b096d65b
Ahmed ELsayed <Ahmed ELsayed>
2017-02-01 23:05:31 +0200

    shift

--- sites/all/themes/front/templates/page--page_not_found.tpl.php
+++ sites/all/themes/front/templates/page--page_not_found.tpl.php
@@ -8,7 +8,7 @@
 <div class="container" style="width: 100%;margin: auto;">
     <div style="width: 70%;;margin-right: auto; margin-left: auto;">
         <a href="<?php echo $GLOBALS['base_url'];?>" style="cursor: pointer;">
-            <img style="width: 100%;max-height: 100%;margin-right: auto; margin-left: auto;" class="" src="<?php echo $GLOBALS['base_url'].'/'.path_to_theme();?>/img/404-Error.jpg" />
+            <img style="margin-top: 90px;width: 100%;max-height: 100%;margin-right: auto; margin-left: auto;" class="" src="<?php echo $GLOBALS['base_url'].'/'.path_to_theme();?>/img/404-Error.jpg" />
         </a>
     </div>
 </div>
