9f3b94af25140dfd3e4e448bb8ed0458c9e2c46a
Mohamed Elsayed <m.elsayed@shiftebusiness.com>
2017-02-27 22:29:54 +0200

    shift

--- sites/all/themes/front/templates/home/about.php
+++ sites/all/themes/front/templates/home/about.php
@@ -5,13 +5,17 @@
 		$title = $item->title;
 	}	
 	if(isset($item->body[LANGUAGE_NONE][0]['value'])){
-		$body = $item->body[LANGUAGE_NONE][0]['value'];
-	}?>
+		$body = elsayed_cut_string_words($item->body[LANGUAGE_NONE][0]['value'], 50);
+	}
+	$item_url = elsayed_get_node_url_by_id($item->nid);?>
 	<div id="about" class="cta-1">
 		<div class="container">
 			<div class="row text-center white">
 				<h1 class="cta-title"><?php echo $title;?></h1>
 				<div class="cta-sub-title"><?php echo $body;?></div>
+				<div class="col-md-12">
+	            	<a class="inpt" href="<?php echo $item_url;?>"><?php echo __('المزيد');?></a>
+	            </div>
 			</div>
 		</div>
 	</div>
